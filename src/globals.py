384c96abcafbbf0124e669a540fdc7994b9418ff
cxnt <pavelbartsits@gmail.com>
2022-12-13 22:05:22 +0400

    set default framerate

--- src/globals.py
+++ src/globals.py
@@ -26,6 +26,6 @@ STORAGE_DIR = os.path.join(sly.app.get_data_dir(), "video")
 mkdir(STORAGE_DIR, True)
 
 DOWNLOAD_THRESHOLD = 0.25
-FRAME_RATE = 30
+FRAME_RATE = 24
 FULL_VIDEO = "full_video"
 BY_FRAME = "by_frame"
